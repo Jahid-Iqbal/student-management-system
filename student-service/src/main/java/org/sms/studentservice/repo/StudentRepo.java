52ab0cf94d09ae40ba35174831e4ff6a3a3d0604
Jahid-Iqbal <jahidiqbal1994@gmail.com>
2026-02-17 16:10:23 +0600

    exception intergrate

--- student-service/src/main/java/org/sms/studentservice/repo/StudentRepo.java
+++ student-service/src/main/java/org/sms/studentservice/repo/StudentRepo.java
@@ -3,5 +3,9 @@ package org.sms.studentservice.repo;
 import org.sms.studentservice.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepo extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByEmail(String email);
 }
